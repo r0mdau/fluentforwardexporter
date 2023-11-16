a2b647aac32a37f891cad79537041335fc0eb80e
Romain Dauby <romain.dauby@gmail.com>
2023-11-15 21:01:48 -0500

    First fluentforwardexporter iteration

---
+++ config.go
@@ -0,0 +1,43 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package fluentforwardexporter // import "github.com/r0mdau/fluentforwardexporter"
+
+import (
+	"fmt"
+	"net/url"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config/configtls"
+	"go.opentelemetry.io/collector/exporter/exporterhelper"
+)
+
+type TCPClientSettings struct {
+	// The target URL to send data to (e.g.: http://some.url:9411/v1/traces).
+	Endpoint string `mapstructure:"endpoint"`
+
+	// TLSSetting struct exposes TLS client configuration.
+	TLSSetting configtls.TLSClientSetting `mapstructure:"tls"`
+}
+
+// Config defines configuration for fluentforward exporter.
+type Config struct {
+	TCPClientSettings            `mapstructure:",squash"` // squash ensures fields are correctly decoded in embedded struct.
+	exporterhelper.QueueSettings `mapstructure:"sending_queue"`
+	exporterhelper.RetrySettings `mapstructure:"retry_on_failure"`
+
+	DefaultLabelsEnabled map[string]bool `mapstructure:"default_labels_enabled"`
+}
+
+var _ component.Config = (*Config)(nil)
+
+func (config *Config) Validate() error {
+	if err := config.QueueSettings.Validate(); err != nil {
+		return fmt.Errorf("queue settings has invalid configuration: %w", err)
+	}
+
+	if _, err := url.Parse(config.Endpoint); config.Endpoint == "" || err != nil {
+		return fmt.Errorf("\"endpoint\" must be a valid URL")
+	}
+	return nil
+}
---
+++ factory.go
@@ -0,0 +1,54 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package fluentforwardexporter // import "github.com/r0mdau/fluentforwardexporter"
+
+import (
+	"context"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/exporter"
+	"go.opentelemetry.io/collector/exporter/exporterhelper"
+
+	"github.com/r0mdau/fluentforwardexporter/internal/metadata"
+)
+
+// NewFactory creates a factory for the fluentforward exporter.
+func NewFactory() exporter.Factory {
+	// later count failed log records
+	//_ = view.Register(metricViews()...)
+
+	return exporter.NewFactory(
+		metadata.Type,
+		createDefaultConfig,
+		exporter.WithLogs(createLogsExporter, metadata.LogsStability),
+	)
+}
+
+func createDefaultConfig() component.Config {
+	return &Config{
+		TCPClientSettings: TCPClientSettings{
+			Endpoint: "localhost:24224",
+		},
+		RetrySettings: exporterhelper.NewDefaultRetrySettings(),
+		QueueSettings: exporterhelper.NewDefaultQueueSettings(),
+	}
+}
+
+func createLogsExporter(ctx context.Context, set exporter.CreateSettings, config component.Config) (exporter.Logs, error) {
+	exporterConfig := config.(*Config)
+	exp := newExporter(exporterConfig, set.TelemetrySettings)
+
+	return exporterhelper.NewLogsExporter(
+		ctx,
+		set,
+		config,
+		exp.pushLogData,
+		// explicitly disable since we rely on http.Client timeout logic.
+		exporterhelper.WithTimeout(exporterhelper.TimeoutSettings{Timeout: 0}),
+		exporterhelper.WithRetry(exporterConfig.RetrySettings),
+		exporterhelper.WithQueue(exporterConfig.QueueSettings),
+		exporterhelper.WithStart(exp.start),
+		exporterhelper.WithShutdown(exp.stop),
+	)
+}
